25d92ab439fc7434704711de738b83efdca95a96
b0r1ngx <ghztrikz1337@gmail.com>
2022-03-20 02:34:04 +0300

    Initial commit

---
+++ app/src/main/java/b0r1ngx/maketricks/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package b0r1ngx.maketricks.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
